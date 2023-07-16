08c91d100ee2c0c12ad49483388ff8e020655c0f
ciptoardiantoro <ciptoardianto70@gmail.com>
2023-07-16 14:18:23 +0700

    Perubahan navbar

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#242424",
+      },
+    },
+  },
+  plugins: [],
+};
